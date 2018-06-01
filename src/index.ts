8307c68a0fbaabfbe0603d3ba80576f6199f1ccd
James Wright <james.wright@yld.io>
2018-06-01 15:49:56 +0100

    Adds images, minor code tweaks

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ const searchInput = searchForm.querySelector<HTMLInputElement>('#search-input');
 const canvas = document.body.querySelector<HTMLCanvasElement>('#output');
 const context = canvas.getContext('2d');
 
-searchForm.addEventListener('submit', e => {
+searchForm.addEventListener('submit', async e => {
     e.preventDefault();
+
+    const { value } = searchInput;
+    const response = await fetch(`/api/weather/${value}`);
+    const result = await response.json();
+
+    // tslint:disable-next-line:no-console
+    console.log('******', JSON.stringify(result, null, 4));
 });

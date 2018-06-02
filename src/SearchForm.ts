bc1063823e3c8fe6b1f8c7b477a4cd22786fcc63
James Wright <james@jamesswright.co.uk>
2018-06-02 19:48:56 +0100

    Adds font, WIP styling

--- src/SearchForm.ts
+++ src/SearchForm.ts
@@ -7,6 +7,7 @@ class SearchForm {
     private _onResult = (result: Result) => undefined;
 
     constructor(form: HTMLFormElement) {
+        this._searchInput = form.querySelector('.search-form__input');
         form.addEventListener('submit', this.search);
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import SearchForm from './SearchForm';
 import App from './App';
 
-const searchFormElement = document.body.querySelector<HTMLFormElement>('#search-form');
+const searchFormElement = document.body.querySelector<HTMLFormElement>('.search-form');
 const canvas = document.body.querySelector<HTMLCanvasElement>('#output');
 const context = canvas.getContext('2d');
 const searchForm = new SearchForm(searchFormElement);

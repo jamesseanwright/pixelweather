8b7006571baff8f36541368ced51a0bd495080c9
James Wright <james@jamesswright.co.uk>
2018-06-03 19:56:31 +0100

    Toggles cloud colour based upon weather

--- src/outputObjects/Cloud.ts
+++ src/outputObjects/Cloud.ts
@@ -1,10 +1,13 @@
 import ImageSprite from './ImageSprite';
+import Result from '../Result';
 
 class Cloud extends ImageSprite {
+    public static getImagePath = (isGrey: boolean) => `/images/cloud${isGrey ? '-grey' : ''}.png`;
+
     constructor(context: CanvasRenderingContext2D, isGrey: boolean, x: number, y: number, xSpeed: number) {
         super(
             context,
-            `/images/cloud${isGrey ? '-grey' : ''}.png`,
+            Cloud.getImagePath(isGrey),
             x,
             y,
             120,
@@ -17,6 +20,14 @@ class Cloud extends ImageSprite {
          * to acquire new behaviour results in breaking change
          * to required parameters. Also receiving too many params
          */
+    }
+
+    public setState(state: Result) {
+        super.setState(state);
+
+        const { weather } = state;
+        const isGrey = weather.some(({ main }) => main === 'Rain' || main === 'Thunder');
+        this.setImage(Cloud.getImagePath(isGrey));
     }
 }
 
--- src/outputObjects/ImageSprite.ts
+++ src/outputObjects/ImageSprite.ts
@@ -1,15 +1,21 @@
 import Moveable from './Moveable';
 import Loopable from './Loopable';
 
-const createImage = (src: string) => {
-    const image = new Image();
-    image.src = src;
-    return image;
-};
-
+// TODO: => ImageRenderable
 class ImageSprite extends Loopable {
+    // TODO: preload images once and cache
+    public static createImage = async (src: string) => (
+        new Promise<HTMLImageElement>((resolve, reject) => {
+            const image = new Image();
+            image.onload = () => resolve(image);
+            image.onerror = reject;
+            image.src = src;
+        })
+    )
+
     private _context: CanvasRenderingContext2D;
     private _image: HTMLImageElement;
+    private _hasImageLoaded = false;
 
     constructor(
         context: CanvasRenderingContext2D,
@@ -24,19 +30,31 @@ class ImageSprite extends Loopable {
     ) {
         super(x, y, width, height, xSpeed, ySpeed, maxX);
         this._context = context;
-        this._image = createImage(src);
+        this.loadImage(src);
     }
 
     public next() {
         super.next();
 
-        this._context.drawImage(
-            this._image,
-            this.x,
-            this.y,
-            this.width,
-            this.height,
-        );
+        if (this._hasImageLoaded) {
+            this._context.drawImage(
+                this._image,
+                this.x,
+                this.y,
+                this.width,
+                this.height,
+            );
+        }
+    }
+
+    protected setImage = async (src: string) => {
+        this.loadImage(src);
+    }
+
+    private loadImage = async (src: string) => {
+        this._hasImageLoaded = false;
+        this._image = await ImageSprite.createImage(src);
+        this._hasImageLoaded = true;
     }
 }
 

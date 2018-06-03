98670a8eabd3eb133da9fa56701edd33b90e323f
James Wright <james@jamesswright.co.uk>
2018-06-03 19:23:19 +0100

    Removes complex visual state boilerplate

--- src/App.ts
+++ src/App.ts
@@ -8,30 +8,22 @@ import OutputObject from './outputObjects/OutputObject';
 import Person from './outputObjects/Person';
 import Cloud from './outputObjects/Cloud';
 import Road from './outputObjects/Road';
-import getVisualState from './visualStates';
 
 class App {
     private _metadataView: MetadataView;
-    private _outputObjects: Map<string, OutputObject[]>;
+    private _outputObjects: OutputObject[];
 
     constructor(context: CanvasRenderingContext2D, searchForm: SearchFormView, metadata: MetadataView) {
-        this._outputObjects = new Map<string, OutputObject[]>([
-            ['background', [Background.clearSky(context)]],
-
-            ['clouds', [
-                new Cloud(context, false, 720, 70, -0.5),
-                new Cloud(context, false, 400, 100, -0.75),
-                new Cloud(context, false, 150, 150, -1),
-            ]],
-
-            ['road', [new Road(context, 0, 520)]],
-
-            ['people', [
-                new Person(context, '/images/person-1.png', 60, 450),
-                new Person(context, '/images/person-2.png', 110, 450),
-                new Person(context, '/images/person-3.png', 160, 450),
-            ]],
-        ]);
+        this._outputObjects = [
+            Background.clearSky(context),
+            new Cloud(context, false, 720, 70, -0.5),
+            new Cloud(context, false, 400, 100, -0.75),
+            new Cloud(context, false, 150, 150, -1),
+            new Road(context, 0, 520),
+            new Person(context, '/images/person-1.png', 60, 450),
+            new Person(context, '/images/person-2.png', 110, 450),
+            new Person(context, '/images/person-3.png', 160, 450),
+        ];
 
         this._metadataView = metadata;
 
@@ -39,15 +31,12 @@ class App {
     }
 
     public next() {
-        for (const [, outputObjects] of this._outputObjects) {
-            for (const outputObject of outputObjects) {
-                outputObject.next();
-            }
+        for (const outputObject of this._outputObjects) {
+            outputObject.next();
         }
     }
 
     private onResult = (result: Result) => {
-        this._outputObjects = getVisualState(result, this._outputObjects);
         this._metadataView.update(result);
     }
 }

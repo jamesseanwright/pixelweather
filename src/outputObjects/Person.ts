0d99f3cac4d3a8d2cbdf5ea16b356f9e8a580ff8
James Wright <james@jamesswright.co.uk>
2018-06-08 18:29:28 +0100

    Deletes old output objects

--- src/outputObjects/Person.ts
+++
@@ -1,9 +0,0 @@
-import ImageRenderable from './ImageRenderable';
-
-class Person extends ImageRenderable {
-    constructor(context: CanvasRenderingContext2D, image: string, x: number, y: number) {
-        super(context, image, x, y, 72, 72, 0, 0, 0);
-    }
-}
-
-export default Person;
